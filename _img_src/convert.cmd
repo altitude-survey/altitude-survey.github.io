5f77e5b198e21aeae6cb3fa2fda32d0f7dce9d59
Serg <48957210+skalasb@users.noreply.github.com>
2020-07-12 05:45:10 +0400

    картинки уменьшил (первая итерация)

---
+++ _img_src/convert.cmd
@@ -0,0 +1,19 @@
+@echo off
+set magic="C:\Program Files\ImageMagick-7.0.10-Q16-HDRI\magick.exe"
+:: xcopy /t /e
+goto :end
+
+for /D %%a in (1.2, 1.3, 2.1, 2.2, 3.0, 3.2) do (
+echo ------- %%a
+%magic% "gal%%a/*.jpg" -set filename:f %%t -resize 240  "../_temp/gal%%a/%%[filename:f].jpg"
+)
+for /D %%a in (3.1, 3.3, 3.4, 3.5, 3.6, 4.0) do (
+echo ------- %%a
+%magic% "gal%%a/*.jpg" -set filename:f %%t -resize 400  "../_temp/gal%%a/%%[filename:f].jpg"
+)
+
+:end
+
+ 
+forfiles /p "..\_temp" /m *.jpg  /s /c "%cd%\ect.exe -9 -strip -p  @path"
+forfiles /p "..\_temp" /m *.png  /s /c "%cd%\ect.exe -9 -strip -p  @path"
