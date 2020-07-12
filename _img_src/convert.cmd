480c657600aefeafc5384c7e567c33b8846bfc1b
Serg <48957210+skalasb@users.noreply.github.com>
2020-07-12 06:00:38 +0400

    поправка для gal3.2

--- _img_src/convert.cmd
+++ _img_src/convert.cmd
@@ -3,11 +3,11 @@ set magic="C:\Program Files\ImageMagick-7.0.10-Q16-HDRI\magick.exe"
 :: xcopy /t /e
 goto :end
 
-for /D %%a in (1.2, 1.3, 2.1, 2.2, 3.0, 3.2) do (
+for /D %%a in (1.2, 1.3, 2.1, 2.2, 3.0) do (
 echo ------- %%a
 %magic% "gal%%a/*.jpg" -set filename:f %%t -resize 240  "../_temp/gal%%a/%%[filename:f].jpg"
 )
-for /D %%a in (3.1, 3.3, 3.4, 3.5, 3.6, 4.0) do (
+for /D %%a in (3.1, 3.3, 3.4, 3.5, 3.6, 4.0, 3.2 ) do (
 echo ------- %%a
 %magic% "gal%%a/*.jpg" -set filename:f %%t -resize 400  "../_temp/gal%%a/%%[filename:f].jpg"
 )
