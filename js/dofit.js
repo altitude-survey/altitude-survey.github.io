e9dff6859ec9e64312fcc0a86dea341a36383666
Serg <48957210+skalasb@users.noreply.github.com>
2020-10-01 16:40:47 +0400

    .

--- js/dofit.js
+++ js/dofit.js
@@ -4,9 +4,10 @@
     // console.log('doFit');
     textFit(document.querySelectorAll(".uk-overlay h4"));
   }
-  window.onload = doFit;
-  window.onresize = doFit;
+  // window.onload = doFit;
+  // window.onresize = doFit;
   //UIkit.util.on(document, 'DOMContentLoaded',doFit);
   document.addEventListener("DOMContentLoaded", doFit);
+  document.addEventListener("resize", doFit);
   if (document.readyState == "complete") doFit();
 })();
