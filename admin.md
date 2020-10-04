c61fd43b020d1355ae302841efa09e9c391c40fb
Serg <48957210+skalasb@users.noreply.github.com>
2020-10-04 22:05:57 +0400

    .

--- admin.md
+++ admin.md
@@ -5,5 +5,7 @@ sitemap: false
 ---
 
 * [анализ сайта pr-cy.ru](https://a.pr-cy.ru/высотноеобследование.рф/)
+* [Яндекс Вебмастер](https://webmaster.yandex.ru/site/https:высотноеобследование.рф:443/dashboard/)
+* [Google Search Console](https://search.google.com/search-console?hl=ru&resource_id=sc-domain:xn--80abcbjebd0azthebbe5bgn1p.xn--p1ai)
 
 
--- js/dofit.js
+++ js/dofit.js
@@ -3,10 +3,11 @@
   script.src = "/js/textFit.min.js";
   script.onload = () => {
     function doFit() {
-      console.log("doFit");
+      console.log("doFit:",doFit.caller);
       textFit(document.querySelectorAll(".uk-overlay h4"));
     }
     window.addEventListener("resize", doFit);
+    window.addEventListener('orientationchange', doFit);
     if (document.readyState == "loading") {
       document.addEventListener("DOMContentLoaded", doFit);
     } else {
