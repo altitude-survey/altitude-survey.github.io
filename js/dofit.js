4c0407451b389dd066dbb11bf85c452977913d12
Serg <48957210+skalasb@users.noreply.github.com>
2020-11-08 20:51:39 +0400

    Update dofit.js

--- js/dofit.js
+++ js/dofit.js
@@ -3,18 +3,36 @@
   script.src = "/js/textFit.min.js";
   script.onload = (e) => {
     function doFit(ev) {
-      console.log("doFit:",ev.type);
+      console.log("all Fit on: ",ev.type);
       textFit(document.querySelectorAll(".uk-overlay h4"));
       let la=document.getElementById("logo-anch");
       la.style.width=la.offsetHeight+'px';
     }
     window.addEventListener("resize", doFit);
     window.addEventListener('orientationchange', doFit);
-    if (document.readyState == "loading") {
-      document.addEventListener("DOMContentLoaded", doFit);
-    } else {
-      doFit(e);
+    function fitOne(el,reason){
+      textFit(el);
+      console.log(`Fit ${reason}`);
     }
+    document.querySelectorAll(".uk-card-media-top > img")
+      .forEach(x =>
+        x.src ?
+            fitOne(
+              x.parentElement.querySelector("h4"),
+               'exist ' + (new URL(x.src)).pathname
+               )
+          : x.addEventListener("load", e =>
+              fitOne(
+                e.target.parentElement.querySelector("h4"),
+                'onload ' + (new URL(e.target.src)).pathname
+              )
+            )
+      );
+    // if (document.readyState == "loading") {
+    //   document.addEventListener("DOMContentLoaded", doFit);
+    // } else {
+    //   doFit(e);
+    // }
   };
   document.head.append(script);
 })();
